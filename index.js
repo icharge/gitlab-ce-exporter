df8bfcd2fec4c6016bd53ab37a4d63d3e50b8d78
iNore <charge_n@hotmail.com>
2017-08-02 21:44:07 +0700

    Update transformer function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const unirest = require('unirest');
 const parser = require('xml2json');
 
 const targetXml = 'example.xml';
-const targetJson = 'data.json';
+const targetJson = 'data2.json';
 
 function xmlFromFile(targetXml) {
   // Read file for example
@@ -31,8 +31,38 @@ function saveJson(fileName, jsonObj) {
   fs.writeFileSync(path.join(__dirname, fileName), jsonContent);
 }
 
+function transformData(data) {
+  const feed = data.feed;
+
+  /**
+   * @type {Array}
+   */
+  const issues = feed.entry;
+  const transData = {
+    title: feed.title,
+    links: feed.link,
+    updated: feed.updated,
+    issues: issues.map((issue, index) => {
+      return {
+        id: '#' + issue.id.split('/').pop(),
+        title: issue.title,
+        updated: issue.updated,
+        author: issue.author.name,
+        description: issue.description,
+        assignee: issue.assignee && issue.assignee.name || '',
+        labels: issue.labels && issue.labels.label.join(', '),
+        serverity: '',
+        status: '',
+      };
+    }) || [],
+  };
+
+  return transData;
+}
+
 const xmlContent = xmlFromFile(targetXml);
 const jsonObj = xmlToJson(xmlContent);
 
-saveJson(targetJson, jsonObj);
+// saveJson(targetJson, jsonObj);
+saveJson(targetJson, transformData(jsonObj));
 
